d2a731426d0e2d8b1f0b469821c71f7dbda035f1
Dan Hummon <dan.hummon@tartansolutions.com>
2012-12-24 18:16:59 -0500

    Adding a simple collision routine to the engine.
    Nothing uses this yet, but I think it will be useful.

--- app/engine.js
+++ app/engine.js
@@ -104,6 +104,15 @@ defineClass('SMEngine', function(aCanvas) {
     }
   },
 
+  colliding: function(bounds1, bounds2) {
+    if (bounds1[kSMBottom] < bounds2[kSMTop])    return false;
+    if (bounds1[kSMTop]    > bounds2[kSMBottom]) return false;
+    if (bounds1[kSMRight]  < bounds2[kSMLeft])   return false;
+    if (bounds1[kSMLeft]   > bounds2[kSMRight])  return false;
+
+    return true;
+  },
+
   updateViewport: function() {
     //  TODO: follow player's position;
     if (this.tickNumber % 4 == 0 && document.getElementById('auto-scroll').checked) {
